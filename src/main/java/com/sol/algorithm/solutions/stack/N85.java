dad5badfc04c6ce6ad74e3cf8e6f4f18666b3a23
elias.wang <elias.wang@zkteco.com>
2022-01-04 10:52:10 +0800

    添加解题思路说明

--- src/main/java/com/sol/algorithm/solutions/stack/N85.java
+++ src/main/java/com/sol/algorithm/solutions/stack/N85.java
@@ -1,7 +1,6 @@
 package com.sol.algorithm.solutions.stack;
 
 
-import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -13,10 +12,14 @@ public class N85 {
         System.out.println(solution.maximalRectangle(new char[][]{{'1'}}));
     }
 
-    private int maxArea = 0;
-    private int[] heights;
-
     /**
+     * 解题思路基于 LeetCode 84 题。
+     * <p>
+     * 横坐标在 (l, r) 内的一个有效矩形，其面积可以看作 底边【r - l - 1】和 高【Min(heights[h])】
+     * (l < h < r，且 heights[l] < heights[h] < heights[r]) 的乘积。
+     * 如果我们确保 (l, r) 之间的矩形高度单调递增，则 Min(heights[h]) = heights[l+1]。
+     * 实现时，因为我们从左到右遍历输入矩阵，所以先确定右边界，再动态调整左边界计算有效矩形的面积。
+     *
      * <li>时间复杂度：O(m*n)</li>
      * <li>空间复杂度：O(n)</li>
      */
@@ -29,38 +32,30 @@ public class N85 {
         // 首位增加0，作为第一个矩形的左边界，避免在循环中检查栈是否为空
         // 末位增加0，作为最后一个矩形的右边界
         n += 2;
-        heights = new int[n];
+        int[] heights = new int[n];
 
+        int maxArea = 0;
         for (int i = 0; i < m; i++) {
             indexes = new Stack<>();
             indexes.push(0);
             for (int j = 1; j < n; j++) {
-                // 转换每行的高度数组，注意j在【matrix】中的对应
+                // 转换每行的高度数组，注意 j 在 matrix 中的对应
                 if (j < n - 1 && matrix[i][j - 1] == '1') {
                     heights[j] += 1;
                 } else {
                     heights[j] = 0;
                 }
-                // 当前高度小于栈顶下标对应的高度时，j为右边界
-                calcMaxArea(indexes, j);
+                // 当前高度小于栈顶下标对应的高度时，j 为右边界
+                while (heights[indexes.peek()] > heights[j]) {
+                    int height = heights[indexes.pop()];
+                    // 新的栈顶元素 indexes.peek() 为左边界
+                    // 这里可以加上条件，找到高度小于待计算矩形的严格左边界：while (heights[indexes.peek()] == height) indexes.pop();
+                    int width = j - indexes.peek() - 1;
+                    maxArea = Math.max(maxArea, width * height);
+                }
                 indexes.push(j);
             }
         }
         return maxArea;
-    }
-
-    /**
-     * 计算高度单调增加的矩形区域的最大面积
-     *
-     * @param indexes 高度单调增加的矩形下标栈，其 栈底下标对应高度必定小于右边界对应高度
-     * @param r       右边界下标
-     */
-    private void calcMaxArea(Stack<Integer> indexes, int r) {
-        while (heights[indexes.peek()] > heights[r]) {
-            int height = heights[indexes.pop()];
-            // 新的栈顶元素 indexes.peek() 为左边界
-            int width = r - indexes.peek() - 1;
-            maxArea = Math.max(maxArea, width * height);
-        }
     }
 }
