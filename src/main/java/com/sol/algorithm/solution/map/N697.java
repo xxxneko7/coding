3097a9e5ab59751cdbec81dc606d5f7dd72fd55b
elias.wang <elias.wang@zkteco.com>
2022-01-04 18:41:52 +0800

    添加 N697

---
+++ src/main/java/com/sol/algorithm/solution/map/N697.java
@@ -0,0 +1,18 @@
+package com.sol.algorithm.solution.map;
+
+
+/**
+ * 697. 数组的度
+ */
+public class N697 {
+
+    /**
+     * n为【nums】的长度
+     * <li> 时间复杂度：O(n) </li>
+     * <li> 空间复杂度：O(n) </li>
+     */
+
+    public int findShortestSubArray(int[] nums) {
+        return 0;
+    }
+}
