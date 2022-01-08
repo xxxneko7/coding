83b618fdf70eb50e8c516945d4392bafdba8e21b
elias.wang <elias.wang@zkteco.com>
2022-01-08 18:38:20 +0800

    添加注释

--- src/main/java/com/sol/algorithm/solution/map/N30.java
+++ src/main/java/com/sol/algorithm/solution/map/N30.java
@@ -2,30 +2,56 @@ package com.sol.algorithm.solution.map;
 
 import java.util.*;
 
+/**
+ * 30. 串联所有单词的子串
+ */
 public class N30 {
-    public static void main(String[] args) {
-        System.out.println(new N30().findSubstring("barfoothefoobarman", new String[]{"foo", "bar"}));
-    }
+    /**
+     * s 的字符数组
+     */
+    private char[] chars;
 
+    /**
+     * 单词长度
+     */
+    private int lenOfWord;
+
+    /**
+     * 统计单词出现次数的映射
+     */
+    Map<String, Integer> wordToNum;
+
+    /**
+     * - 时间复杂度：O()
+     * - 空间复杂度：O()
+     *
+     * @param s     字符串
+     * @param words 单词数组 - 数组中单词长度相同
+     * @return s 中恰好可以由 words 中所有单词串联形成的子串的起始位置 - 不需要考虑 words 中单词串联的顺序
+     */
     public List<Integer> findSubstring(String s, String[] words) {
-        Map<String, Integer> wordToNum = new HashMap<>(words.length);
+        wordToNum = new HashMap<>(words.length);
         Arrays.stream(words).forEach(word -> wordToNum.put(word, wordToNum.getOrDefault(word, 0) + 1));
-        char[] chars = s.toCharArray();
-        int lenOfWord = words[0].length();
+        chars = s.toCharArray();
+        lenOfWord = words[0].length();
         int lenOfSubString = lenOfWord * words.length;
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i <= chars.length - lenOfSubString; ) {
-            if (validSubstring(chars, i, i + lenOfSubString, lenOfWord, wordToNum)) {
+        for (int i = 0; i <= chars.length - lenOfSubString; i++) {
+            if (validSubstring(i, i + lenOfSubString)) {
                 res.add(i);
-                i += lenOfWord;
-            } else {
-                i++;
             }
         }
         return res;
     }
 
-    private boolean validSubstring(char[] chars, int start, int end, int lenOfWord, Map<String, Integer> wordToNum) {
+    /**
+     * 验证 chars 的子串是否有效
+     *
+     * @param start 子串的起始位置
+     * @param end   子串的结束位置
+     * @return true: 有效 | false: 无效
+     */
+    private boolean validSubstring(int start, int end) {
         Map<String, Integer> splitWordToNum = new HashMap<>(wordToNum.size());
         for (int i = start; i + lenOfWord <= end; i += lenOfWord) {
             String splitWord = new String(Arrays.copyOfRange(chars, i, i + lenOfWord));
@@ -34,7 +60,14 @@ public class N30 {
         return compareMap(splitWordToNum, wordToNum);
     }
 
-    private boolean compareMap(Map<String, Integer> a, Map<String, Integer> b) {
+    /**
+     * 比较两个集合
+     *
+     * @param a 集合A
+     * @param b 集合B
+     * @return true: 相同 | false: 不同
+     */
+    private <K, V> boolean compareMap(Map<K, V> a, Map<K, V> b) {
         return a.entrySet().containsAll(b.entrySet());
     }
 }
