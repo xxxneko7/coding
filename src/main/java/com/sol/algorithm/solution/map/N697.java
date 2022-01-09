914b935c3aa5a7750f57fb836eb3fec84964b1cb
Observer <xxx-adonis@qq.com>
2022-01-10 00:04:09 +0800

    修改注释

--- src/main/java/com/sol/algorithm/solution/map/N697.java
+++ src/main/java/com/sol/algorithm/solution/map/N697.java
@@ -9,37 +9,20 @@ import java.util.Map;
  */
 public class N697 {
     public static void main(String[] args) {
-        System.out.println(new N697().findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2}));
-    }
-
-    private class SubArray {
-        /**
-         * 数出现的频数
-         */
-        int freq;
-        /**
-         * 数第一次出现的下标
-         */
-        int firstIdx;
-        /**
-         * 子数组的长度
-         */
-        int len;
-
-        public SubArray(int firstIdx) {
-            this.firstIdx = firstIdx;
-            this.freq = 1;
-        }
+        System.out.println(new N697().findShortestSubArray(new int[]{1}));
     }
 
     /**
-     * n为【nums】的长度
-     * <li> 时间复杂度：O(n) </li>
-     * <li> 空间复杂度：O(n) </li>
+     * n为【nums】的长度 <br>
+     * - 时间复杂度：O(n) <br>
+     * - 空间复杂度：O(n) <br>
+     *
+     * @param nums 非空且只包含非负数的整数数组
+     * @return 与 nums 度相同的 最短连续子数组 的长度
      */
     public int findShortestSubArray(int[] nums) {
-        // 数到子数组对象的映射，用来统计数出现的频数和子数组的长度
         int n = nums.length;
+        // 某个数到其子数组对象的映射，用来统计数出现的频数和子数组的长度
         Map<Integer, SubArray> numToSubArray = new HashMap<>(n);
         for (int i = 0; i < n; i++) {
             SubArray subArray = numToSubArray.get(nums[i]);
@@ -50,7 +33,7 @@ public class N697 {
                 subArray.len = i - subArray.firstIdx + 1;
             }
         }
-        // 找到数出现频数最大的度，及其对应最短子数组的长度
+        // 找到数组的度，及其对应最短子数组的长度
         int degree = 0, minLen = n;
         for (SubArray subArray : numToSubArray.values()) {
             if (degree < subArray.freq) {
@@ -61,5 +44,38 @@ public class N697 {
             }
         }
         return minLen;
+    }
+
+    /**
+     * 以数组中某个数开始和结束的子数组
+     */
+    private class SubArray {
+        /**
+         * 数出现的频数
+         */
+        int freq;
+        /**
+         * 子数组的起始下标
+         */
+        int firstIdx;
+        /**
+         * 子数组的长度
+         */
+        int len;
+
+        public SubArray(int firstIdx) {
+            this.firstIdx = firstIdx;
+            this.freq = 1;
+            this.len = 1;
+        }
+
+        @Override
+        public String toString() {
+            return "SubArray{" +
+                    "freq=" + freq +
+                    ", firstIdx=" + firstIdx +
+                    ", len=" + len +
+                    '}';
+        }
     }
 }
