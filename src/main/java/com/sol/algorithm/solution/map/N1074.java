7e1f20ce73bae6481b5d983100f531b9982de80a
Observer <xxx-adonis@qq.com>
2022-01-10 00:04:39 +0800

    修改注释

--- src/main/java/com/sol/algorithm/solution/map/N1074.java
+++ src/main/java/com/sol/algorithm/solution/map/N1074.java
@@ -15,45 +15,47 @@ public class N1074 {
     }
 
     /**
-     * m、n分别为【matrix】的行、列数 <br/>
-     * <pre>
-     *             r             n
-     *       + - - - - - - - - - +
-     *     t | x x x x x x x x x |
-     *       | - - - - - - - - - |
-     *       |       |           |
-     *       |       |           |
-     *     b |       |           |
-     *       | - - - - - - - - - |
-     *       | x x x x x x x x x |
-     *     m + - - - - - - - - - +
-     * </pre>
+     * 解题思路基于 LeetCode 560. 和为 K 的子数组 <br>
      *
-     * - 时间复杂度：O(m^2 * n) <br/>
-     * - 空间复杂度：O(m) <br/>
+     * <p>
+     * m、n分别为【matrix】的行、列数 <br>
+     * - 时间复杂度：O(m^2 * n) <br>
+     * - 空间复杂度：O(m) <br>
+     *
+     * @param matrix 矩阵
+     * @param target 目标值
+     * @return 元素总和等于目标值的非空子矩阵的数量
      */
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
         int count = 0;
+        // 遍历上界t
         for (int t = 0; t < m; t++) {
-            int[] sumOfCol = new int[n];
+            int[] sumsOfCols = new int[n];
+            // 遍历下界b
             for (int b = t; b < m; b++) {
                 for (int r = 0; r < n; r++) {
-                    sumOfCol[r] += matrix[b][r];
+                    // 计算上下界中每一列的元素和
+                    sumsOfCols[r] += matrix[b][r];
                 }
-                count += subarraySum(sumOfCol, target);
+                count += subarraySum(sumsOfCols, target);
             }
         }
         return count;
     }
 
-    public int subarraySum(int[] nums, int k) {
+    /**
+     * @param nums 数组
+     * @param t    目标值
+     * @return 元素和等于目标值的非空子数组的数量
+     */
+    public int subarraySum(int[] nums, int t) {
         Map<Integer, Integer> preSumToNum = new HashMap<>(nums.length);
         preSumToNum.put(0, 1);
         int count = 0, preSum = 0;
         for (int num : nums) {
             preSum += num;
-            count += preSumToNum.getOrDefault(preSum - k, 0);
+            count += preSumToNum.getOrDefault(preSum - t, 0);
             preSumToNum.put(preSum, preSumToNum.getOrDefault(preSum, 0) + 1);
         }
         return count;
