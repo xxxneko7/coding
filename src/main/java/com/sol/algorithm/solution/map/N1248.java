170b38bf41703974840b9678fdcbefb074746723
Observer <xxx-adonis@qq.com>
2022-01-10 00:39:41 +0800

    1248. 统计「优美子数组」

---
+++ src/main/java/com/sol/algorithm/solution/map/N1248.java
@@ -0,0 +1,32 @@
+package com.sol.algorithm.solution.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1248. 统计「优美子数组」
+ */
+public class N1248 {
+
+    /**
+     * n为【nums】的长度 <br>
+     * - 时间复杂度：O(n) <br>
+     * - 空间复杂度：O(n) <br>
+     *
+     * @param nums 整数数组
+     * @param k    整数
+     * @return 优美子数组的个数 - 「优美子数组」为恰好有 k 个奇数的连续子数组
+     */
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> preSumToNum = new HashMap<>(n + 1, 1);
+        preSumToNum.put(0, 1);
+        int count = 0, preSum = 0;
+        for (int num : nums) {
+            preSum += (num % 2);
+            count += preSumToNum.getOrDefault(preSum - k, 0);
+            preSumToNum.put(preSum, preSumToNum.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+}
