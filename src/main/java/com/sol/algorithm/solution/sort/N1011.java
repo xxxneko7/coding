2dc6e9abdc0dde6f9f6aa87df441eac11fb81ff0
elias.wang <elias.wang@zkteco.com>
2022-01-25 18:30:56 +0800

    1011. 在 D 天内送达包裹的能力

---
+++ src/main/java/com/sol/algorithm/solution/sort/N1011.java
@@ -0,0 +1,10 @@
+package com.sol.algorithm.solution.sort;
+
+/**
+ * 1011. 在 D 天内送达包裹的能力
+ */
+public class N1011 {
+    public int shipWithinDays(int[] weights, int days) {
+        return 0;
+    }
+}
