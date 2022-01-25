0ba7534d605bb2478c3ef98af130e8ca6425d2f7
elias.wang <elias.wang@zkteco.com>
2022-01-25 18:31:03 +0800

    911. 在线选举

---
+++ src/main/java/com/sol/algorithm/solution/sort/N911.java
@@ -0,0 +1,17 @@
+package com.sol.algorithm.solution.sort;
+
+/**
+ * 911. 在线选举
+ */
+public class N911 {
+    class TopVotedCandidate {
+
+        public TopVotedCandidate(int[] persons, int[] times) {
+
+        }
+
+        public int q(int t) {
+            return 0;
+        }
+    }
+}
