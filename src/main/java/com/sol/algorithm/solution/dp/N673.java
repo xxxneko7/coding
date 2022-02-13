12debc6517afed04d998dee0ac68f93927d12db2
Observer <xxx-adonis@qq.com>
2022-02-13 17:38:24 +0800

    更新

--- src/main/java/com/sol/algorithm/solution/dp/N673.java
+++ src/main/java/com/sol/algorithm/solution/dp/N673.java
@@ -5,20 +5,68 @@ package com.sol.algorithm.solution.dp;
  */
 public class N673 {
     /**
-     * 时间复杂度：O() <br>
-     * 空间复杂度：O() <br>
+     * 时间复杂度：O(n^2) <br>
+     * 空间复杂度：O(n) <br>
      *
      * @param nums 无序数组
      * @return 最长递增子序列的个数
      */
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        int l = 1, r = n - 2;
-        while (l < n - 1) {
-            if (nums[l] < nums[l - 1]) l++;
+        Status[] dp = new Status[n];
+        Status LIS = new Status(0, 0);
+        for (int i = 0; i < n; i++) {
+            dp[i] = new Status(1, 1);
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) dp[i] = dp[i].update(dp[j]);
+            }
+            LIS = LIS.compare(dp[i]);
         }
-        while(l<r){
-            if(nums[r]>)
+        return LIS.num;
+    }
+
+    class Status {
+        /**
+         * LIS 长度
+         */
+        int len;
+        /**
+         * LIS 个数
+         */
+        int num;
+
+        Status(int maxLen, int num) {
+            this.len = maxLen;
+            this.num = num;
+        }
+
+        /**
+         * 状态更新
+         *
+         * @param status 历史状态
+         * @return 当前状态
+         */
+        public Status update(Status status) {
+            int newLen = status.len + 1;
+            if (len < newLen) {
+                len = newLen;
+                num = status.num;
+            } else if (len == newLen) {
+                num += status.num;
+            }
+            return this;
+        }
+
+        /**
+         * 状态比较
+         *
+         * @param status 状态
+         * @return LIS 状态
+         */
+        public Status compare(Status status) {
+            if (len < status.len) return status;
+            if (len == status.len) num += status.num;
+            return this;
         }
     }
 }
