ccf18f40dc7f4bae8352def2638862ccef5955b3
Observer <xxx-adonis@qq.com>
2022-03-07 08:18:12 +0800

    第9周

---
+++ src/main/java/com/sol/algorithm/solution/bfs/N1091.java
@@ -0,0 +1,22 @@
+package com.sol.algorithm.solution.bfs;
+
+/**
+ * 1091. 二进制矩阵中的最短路径
+ */
+public class N1091 {
+    /**
+     * 二进制矩阵中的 畅通路径 是一条从 左上角 单元格（即，(0, 0)）到 右下角 单元格（即，(n - 1, n - 1)）的路径，该路径同时满足下述要求：
+     * <li> 路径途经的所有单元格都的值都是 0 </li>
+     * <li> 路径中所有相邻的单元格应当在 8 个方向之一 上连通（即，相邻两单元之间彼此不同且共享一条边或者一个角）</li>
+     * <p>
+     * n 为 grid 的长宽
+     * <li> 时间复杂度：O(n^2) </li>
+     * <li> 空间复杂度：O(n) </li>
+     *
+     * @param grid 二进制矩阵
+     * @return 最短 畅通路径 的长度，不存在时返回 -1
+     */
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+    }
+}
