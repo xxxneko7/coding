b3bbb61d55e4a37806df75a2ca4da114392e8b90
elias.wang <elias.wang@zkteco.com>
2022-03-08 18:37:33 +0800

    1091. 二进制矩阵中的最短路径

--- src/main/java/com/sol/algorithm/solution/bfs/N1091.java
+++ src/main/java/com/sol/algorithm/solution/bfs/N1091.java
@@ -1,22 +1,186 @@
 package com.sol.algorithm.solution.bfs;
 
+import java.util.*;
+
 /**
  * 1091. 二进制矩阵中的最短路径
  */
 public class N1091 {
-    /**
-     * 二进制矩阵中的 畅通路径 是一条从 左上角 单元格（即，(0, 0)）到 右下角 单元格（即，(n - 1, n - 1)）的路径，该路径同时满足下述要求：
-     * <li> 路径途经的所有单元格都的值都是 0 </li>
-     * <li> 路径中所有相邻的单元格应当在 8 个方向之一 上连通（即，相邻两单元之间彼此不同且共享一条边或者一个角）</li>
-     * <p>
-     * n 为 grid 的长宽
-     * <li> 时间复杂度：O(n^2) </li>
-     * <li> 空间复杂度：O(n) </li>
-     *
-     * @param grid 二进制矩阵
-     * @return 最短 畅通路径 的长度，不存在时返回 -1
-     */
-    public int shortestPathBinaryMatrix(int[][] grid) {
+    public static void main(String[] args) {
+        // 4 
+        // int[][] grid = {{0, 0, 0}, {1, 1, 0}, {1, 1, 0}};
+        // -1
+        // int[][] grid = {{1, 0, 0}, {1, 1, 0}, {1, 1, 0}};
+        // 2
+        int[][] grid = {{0, 1}, {1, 0}};
+        Solution solution = new BFS();
+        System.out.println(solution.shortestPathBinaryMatrix(grid));
+    }
 
+    public static abstract class Solution {
+        /**
+         * 二进制矩阵中的 畅通路径 是一条从 左上角 单元格（即，(0, 0)）到 右下角 单元格（即，(n - 1, n - 1)）的路径，该路径同时满足下述要求：
+         * <li> 路径途经的所有单元格都的值都是 0 </li>
+         * <li> 路径中所有相邻的单元格应当在 8 个方向之一 上连通（即，相邻两单元之间彼此不同且共享一条边或者一个角）</li>
+         *
+         * @param grid 二进制矩阵
+         * @return 最短 畅通路径 的长度
+         */
+        abstract int shortestPathBinaryMatrix(int[][] grid);
+
+        /**
+         * 初始化
+         *
+         * @param grid 二进制矩阵
+         */
+        int init(int[][] grid) {
+            this.n = grid.length;
+            if (grid[0][0] == 1) return -1;
+            if (grid[n - 1][n - 1] == 1) return -1;
+            if (n == 1) return 1;
+
+            grid = extendGrid(grid);
+            this.grid = grid;
+            this.start = new Integer[]{1, 1};
+            this.target = new Integer[]{n, n};
+            return 0;
+        }
+
+        /**
+         * 扩展 grid，使得原矩阵被 1 包裹
+         *
+         * @param grid 二进制矩阵
+         * @return 扩展后的新矩阵
+         */
+        int[][] extendGrid(int[][] grid) {
+            int[][] temp = new int[n + 2][n + 2];
+            for (int i = 0; i <= n + 1; i++) {
+                temp[0][i] = 1;
+                temp[n + 1][i] = 1;
+                temp[i][0] = 1;
+                temp[i][n + 1] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.arraycopy(grid[i], 0, temp[i + 1], 1, n);
+            }
+
+            return temp;
+        }
+
+
+        /**
+         * 二进制矩阵
+         */
+        int[][] grid;
+        /**
+         * 起始位置
+         */
+        Integer[] start;
+        /**
+         * 目标位置
+         */
+        Integer[] target;
+        /**
+         * grid 的长宽
+         */
+        int n;
+        /**
+         * 方向数组 [→, ↓, ←, ↑, ↘, ↗, ↙, ↖]
+         */
+        int[] dx = {1, 0, -1, 0, 1, 1, -1, -1};
+        int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+    }
+
+    /**
+     * 普通广度优先搜索
+     */
+    public static class BFS extends Solution {
+
+        /**
+         * n 为 grid 的长宽
+         * <li> 时间复杂度：O(n^2) </li>
+         * <li> 空间复杂度：O(n) </li>
+         */
+        @Override
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            int init = init(grid);
+            if (init != 0) return init;
+
+            grid = extendGrid(grid);
+
+            Integer[] start = new Integer[]{1, 1};
+            Integer[] target = new Integer[]{n, n};
+            Queue<Integer[]> queue = new LinkedList<>();
+            queue.offer(start);
+            // 添加空对象作为连续两层之间的隔断
+            queue.offer(null);
+            int depth = 1;
+
+            while (!queue.isEmpty()) {
+                Integer[] pos = queue.poll();
+                if (pos == null) {
+                    // 下一层为空时退出
+                    if (queue.peek() == null) break;
+                    // 不为空时深度加 1，并添加新的隔断
+                    depth++;
+                    queue.offer(null);
+                    continue;
+                }
+                if (grid[pos[0]][pos[1]] == 1) continue;
+                if (pos[0].equals(target[0]) && pos[1].equals(target[1])) return depth;
+                grid[pos[0]][pos[1]] = 1;
+                for (int i = 0; i < dx.length; i++) {
+                    Integer[] next = new Integer[]{pos[0] + dx[i], pos[1] + dy[i]};
+                    queue.offer(next);
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    /**
+     * 双向广度优先搜索
+     */
+    public static class BiBFS extends Solution {
+
+        /**
+         * n 为 grid 的长宽
+         * <li> 时间复杂度：O(n * log(n)) </li>
+         * <li> 空间复杂度：O(n) </li>
+         */
+        @Override
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            int init = init(grid);
+            if (init != 0) return init;
+
+            Queue<Integer[]> forwardQueue = new LinkedList<>();
+            forwardQueue.offer(start);
+            // 添加空对象作为连续两层之间的隔断
+            forwardQueue.offer(null);
+            int depth = 1;
+
+            while (!forwardQueue.isEmpty()) {
+                Integer[] pos = forwardQueue.poll();
+                if (pos == null) {
+                    // 下一层为空时退出
+                    if (forwardQueue.peek() == null) break;
+                    // 不为空时深度加 1，并添加新的隔断
+                    depth++;
+                    forwardQueue.offer(null);
+                    continue;
+                }
+                if (grid[pos[0]][pos[1]] == 1) continue;
+                if (pos[0].equals(target[0]) && pos[1].equals(target[1])) return depth;
+                grid[pos[0]][pos[1]] = 1;
+                for (int i = 0; i < dx.length; i++) {
+                    Integer[] next = new Integer[]{pos[0] + dx[i], pos[1] + dy[i]};
+                    forwardQueue.offer(next);
+                }
+            }
+
+            return -1;
+        }
     }
 }
