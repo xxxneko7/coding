5485ed7de6780cf902a107f44b7a879eff3339f2
elias.wang <elias.wang@zkteco.com>
2022-08-08 14:23:13 +0800

    更新

--- src/main/java/com/sol/algorithm/solution/N899.java
+++ src/main/java/com/sol/algorithm/solution/N899.java
@@ -1,10 +1,6 @@
 package com.sol.algorithm.solution;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class N899 {
     public static void main(String[] args) {
@@ -17,33 +13,24 @@ public class N899 {
      * 返回 在应用上述步骤的任意数量的移动后，字典上最小的字符串  。
      */
     public String orderlyQueue(String s, int k) {
-        int l = s.length();
-        if (l <= k) {
-            return s.chars().sorted().boxed().map(String::valueOf).collect(Collectors.joining());
+        if (k == 1) {
+            String smallest = s;
+            StringBuilder sb = new StringBuilder(s);
+            int n = s.length();
+            for (int i = 1; i < n; i++) {
+                char c = sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(c);
+                if (sb.toString().compareTo(smallest) < 0) {
+                    smallest = sb.toString();
+                }
+            }
+            return smallest;
+        } else {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            return new String(chars);
         }
-        PriorityQueue<int[]> queue = new PriorityQueue<>(k, Comparator.comparingInt(a -> a[0]));
-        StringBuilder sb = new StringBuilder();
-        Set<Integer> mark = new HashSet<>();
-        while (!queue.isEmpty()) {
-            int[] q = queue.poll();
-            sb.append(q[0]);
-            mark.add(q[1]);
-        }
-        char[] chars = s.toCharArray();
-        PriorityQueue<Character> min = new PriorityQueue<>();
-        for (int i = 0; i < k; i++) {
-            if (mark.contains(i)) continue;
-            min.offer(chars[i]);
-        }
-        StringBuilder sb1 = new StringBuilder();
-        for (int i = k; i < l; i++) {
-            if (mark.contains(i)||min.isEmpty()) continue;
-            Character ch = min.poll();
-            sb1.append(ch);
-        }
-        return sb.toString();
     }
 
-    // 1 1 2 | j a a
-    //  a 1 b d i 2 j a 1 c d
 }
